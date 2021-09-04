c13c8795f684ccb2664a784f27fc4d96acb0389a
David Beers <33200674+DavidABeers@users.noreply.github.com>
2021-09-04 19:44:29 -0400

    Initial commit

---
+++ src/main/java/Solution04.java
@@ -0,0 +1,12 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 first_name last_name
+ */
+
+
+
+public class Solution04 {
+    public static void main(String []args){
+        
+    }
+}
